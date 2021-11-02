8de6424aa950523ea222590287f8bef60155912f
Sheiss Husnain <sheisswaterloo@gmail.com>
2021-11-02 10:16:09 -0400

    running train to get pkl, fixing api_test

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -6,7 +6,7 @@ from ml.data import process_data
 
 from ml.model import train_model
 
-df = pd.read_csv(r"starter/data/census-clean.csv")
+df = pd.read_csv("../data/census-clean.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(df, test_size=0.20)
@@ -29,6 +29,6 @@ X_train, y_train, encoder, lb = process_data(train, categorical_features=cat_fea
 
 model = train_model(X_train,y_train)
 
-pd.to_pickle(model,'starter/model/model.pkl')
-pd.to_pickle(encoder,'starter/model/encoder.pkl')
-pd.to_pickle(lb,'starter/model/lb.pkl')
+pd.to_pickle(model,'../model/model.pkl')
+pd.to_pickle(encoder,'../model/encoder.pkl')
+pd.to_pickle(lb,'../model/lb.pkl')
