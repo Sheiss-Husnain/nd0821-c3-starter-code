34f192068511bacaf7b4335d473b66a042b97cbd
Sheiss Husnain <sheisswaterloo@gmail.com>
2021-11-02 22:15:07 -0400

    final commit :D

--- starter/post_test.py
+++ starter/post_test.py
@@ -2,38 +2,24 @@ import json
 
 import requests
 data = {
-  "age": 39,
-  "workclass": "State-gov",
-  "fnlgt": 77516,
-  "education": "Bachelors",
-  "education-num": 13,
+  "age": 25,
+  "workclass": "Self-emp-not-inc",
+  "fnlgt": 176756,
+  "education": "HS-grad",
+  "education-num": 9,
   "marital-status": "Never-married",
-  "occupation": "Adm-clerical",
-  "relationship": "Not-in-family",
+  "occupation": "Farming-fishing",
+  "relationship": "Own-child",
   "race": "White",
   "sex": "Male",
-  "capital-gain": 2174,
+  "capital-gain": 0,
   "capital-loss": 0,
   "hours-per-week": 40,
   "native-country": "United-States"
 }
-# data= {
-        # "age": 25,
-        # "workclass": "Self-emp-not-inc",
-        # "fnlgt": 176756,
-        # "education": "HS-grad",
-        # "education_num": 9,
-        # "marital_status": "Never-married",
-        # "occupation": "Farming-fishing",
-        # "relationship": "Own-child",
-        # "race": "White",
-        # "sex": "Male",
-        # "capital_gain": 0,
-        # "capital_loss": 0,
-        # "hours_per_week": 35,
-        # "native_country": "United-States"
-#       }
+
 #<=50k
 response = requests.post('https://sheiss-census.herokuapp.com/predict/', json=data)
 #response = client.post("/predict", json=data)
-print(response)
+print(response.status_code)
+print(response.json())
