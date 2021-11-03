9db35ee499c711ff004b01a92c0b34a15e689551
Sheiss Husnain <sheisswaterloo@gmail.com>
2021-11-02 21:36:55 -0400

    just one line

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ lb_ = pd.read_pickle(lb_file)
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    # if os.system("dvc pull") != 0:
-    #     exit("dvc pull failed")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
     # os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 class Input(BaseModel):
